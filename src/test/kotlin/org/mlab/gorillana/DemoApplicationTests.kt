e4f8302dd97d7e7325049bda60807a1f02f9dd21
Masaaki Kodama <tc003ssr@gmail.com>
2019-10-20 17:31:12 +0900

    create kotlin + spring boot project

---
+++ src/test/kotlin/org/mlab/gorillana/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.mlab.gorillana
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
