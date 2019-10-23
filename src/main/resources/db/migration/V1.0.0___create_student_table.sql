753848911217db38d24dc56f316e0dae62171eab
Masaaki Kodama <tc003ssr@gmail.com>
2019-10-23 20:56:41 +0900

    [back] #1 feat: studentsテーブルの定義を変更
    - SERIAL型を外部キーで参照するときはINTEGER型で良い

--- src/main/resources/db/migration/V1.0.0___create_student_table.sql
+++ src/main/resources/db/migration/V1.0.0___create_student_table.sql
@@ -5,8 +5,8 @@ CREATE TABLE students (
   student_number VARCHAR(30) NOT NULL,
   last_name VARCHAR(20) NOT NULL,
   first_name VARCHAR(20) NOT NULL,
-  faculity_id SERIAL NOT NULL,
-  class_id SERIAL NOT NULL,
+  faculity_id INTEGER NOT NULL,
+  class_id INTEGER NOT NULL,
   entrance_year CHAR(4) NOT NULL,
   PRIMARY KEY (student_id)
 );
