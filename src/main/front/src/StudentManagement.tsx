261c238e3762b76cb8f76a11a54af4d234f9814e
konno-yu <yuta.konno.socie@gmail.com>
2019-11-03 20:15:11 +0900

    [front] 「新規作成ボタン」を押したらダイアログが表示されるように処理を追加

--- src/main/front/src/StudentManagement.tsx
+++ src/main/front/src/StudentManagement.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
+import { useContext, useState, useEffect } from 'react';
+import { Store } from './store';
+import { ActionType } from './reducer';
+import { StudentProperties } from './const';
 import { Button } from '@material-ui/core';
 import { InsertDriveFileTwoTone, PersonAddTwoTone } from '@material-ui/icons';
 import TableComponent from './TableComponent';
 
 const StudentManagement = () => {
+    const { state, dispatch } = useContext(Store);
     return (
         <div>
             <div style={{textAlign:"center", margin:"15px 0px"}}>
-                <Button style={{margin:"5px 5px"}} variant="outlined" color="secondary" startIcon={<PersonAddTwoTone/>}>新規作成</Button>
+                <Button style={{margin:"5px 5px"}} variant="outlined" color="secondary" startIcon={<PersonAddTwoTone/>} onClick={() => dispatch({type: ActionType.DIALOG_OPEN})}>新規作成</Button>
                 <Button style={{margin:"5px 5px"}} variant="outlined" startIcon={<InsertDriveFileTwoTone/>}>CSVから新規作成</Button>
-                <TableComponent/>
+                <InputDialog/>
+                <TableComponent>{students}</TableComponent>
             </div>
         </div>
     )
