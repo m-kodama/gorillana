2cc9e02579568eac8d6efcb6e81027a8c2aa3aac
konno-yu <yuta.konno.socie@gmail.com>
2019-11-17 11:22:09 +0900

    [front] 学生データの追加・編集をDBまで貫通

--- src/main/front/src/InputDialog.tsx
+++ src/main/front/src/InputDialog.tsx
@@ -6,38 +6,100 @@ import {
     Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, TextField, InputAdornment, FormControl,
     InputLabel, MenuItem, Select 
 } from '@material-ui/core';
-
+import { StudentProperties, defaultStudent } from './const';
 import { Alphabetical, Numeric } from 'mdi-material-ui'
+import _ from 'lodash';
+import axios from 'axios';
+
+type StudentProps = {
+    children: StudentProperties
+};
 
 
-const InputDialog: React.FC = () => {
+const InputDialog: React.FC<StudentProps> = (props: StudentProps) => {
     const { state, dispatch } = useContext(Store);
+    const [editTargetStudent, setTargetStudent] = useState<StudentProperties>(props.children);
+
+    const dialogMode = (props.children.studentId > 0) ? "編集" : "新規作成";
+
+    const handleTargetStudent = (event: React.ChangeEvent<{value: unknown}>, property: keyof StudentProperties) => {
+        // 念のためコピーに対して更新を実施した後でstateに反映
+        const updateTargetStudent = _.clone(editTargetStudent);
+        switch(property) {
+            case "studentNumber": 
+            case "firstName":
+            case "lastName":
+            case "entranceYear":
+                updateTargetStudent[property] = event.target.value as string;
+                break;
+            case "faculityId":
+            case "classId":
+                updateTargetStudent[property] = event.target.value as number ;
+                break;
+            default: 
+                new Error("予期せぬエラー");
+                break;
+        }
+        setTargetStudent(updateTargetStudent);
+    }
+
+    const postStudent = async () => {
+        await axios.post<StudentProperties[]>("http://localhost:8080/api/students", editTargetStudent).then((response) => {
+            dispatch({
+                type: ActionType.STUDENT_UPDATE, 
+                payload: {...state, students: response.data}
+            });
+        });
+    }
+
+    const putStudent = async () => {
+        const targetStudentId = editTargetStudent.studentId;
+        await axios.put<StudentProperties[]>(`http://localhost:8080/api/students/${targetStudentId}`, editTargetStudent).then((response) => {
+            if(!_.isEmpty(response.data)) {
+                axios.get<StudentProperties[]>("http://localhost:8080/api/students").then((res) => {
+                    dispatch({
+                        type: ActionType.STUDENT_UPDATE,
+                        payload: {...state, students: res.data}
+                    })
+                })
+            }
+        });
+    };
     
-    const [faculityId, changeFaculityId] = useState(0);
-    const [entranceYear, changeEntranceYear] = useState(0);
-
-
-    const handleChangeFaculityId = (event: React.ChangeEvent<{ value: unknown }>) => {
-        changeFaculityId(event.target.value as number);
-    };
-
-    const handleChangeEntranceYear = (event: React.ChangeEvent<{value: unknown}>) => {
-        changeEntranceYear(event.target.value as number);
-    };
-
     return (
         <div>
-            <Dialog open={state.isShowDialog}>
-                <DialogTitle>学生の新規作成</DialogTitle>
+            <Dialog open={true}>
+                <DialogTitle>学生の{dialogMode}</DialogTitle>
                     <DialogContent>
                     <DialogContentText style={{width:400, padding:10}}>
-                        <TextField style={{marginBottom:24, width:"100%"}} label="管理ID" variant="outlined" InputProps={{startAdornment:(<InputAdornment position="start"><Numeric color="action"/> : </InputAdornment>)}}/>
-                        <TextField style={{marginBottom:24, width:"100%"}} label="学籍番号" variant="outlined" InputProps={{startAdornment:(<InputAdornment position="start"><Alphabetical color="action"/> : </InputAdornment>)}}/>
-                        <TextField style={{marginBottom:24, width:"100%"}} label="姓" variant="outlined" InputProps={{startAdornment:(<InputAdornment position="start"><Alphabetical color="action"/> : </InputAdornment>)}}/>
-                        <TextField style={{marginBottom:24, width:"100%"}} label="名" variant="outlined" InputProps={{startAdornment:(<InputAdornment position="start"><Alphabetical color="action"/> : </InputAdornment>)}}/>                        
+                        <TextField disabled={true} value={dialogMode === "編集" ? props.children.studentId : ""} placeholder={dialogMode === "新規作成" ? "自動で入力されます" : ""} style={{marginBottom:24, width:"100%"}} label="管理ID" variant="outlined" InputProps={{startAdornment:(<InputAdornment position="start"><Numeric color="action"/> : </InputAdornment>)}}/>
+                        <TextField 
+                            value={editTargetStudent.studentNumber} 
+                            onChange={(e) => handleTargetStudent(e, "studentNumber")} 
+                            style={{marginBottom:24, width:"100%"}} 
+                            label="学籍番号" 
+                            variant="outlined" 
+                            InputProps={{startAdornment:(<InputAdornment position="start"><Alphabetical color="action"/> : </InputAdornment>)}}
+                        />
+                        <TextField 
+                            value={editTargetStudent.lastName} 
+                            onChange={(e) => handleTargetStudent(e, "lastName")} 
+                            style={{marginBottom:24, width:"100%"}} 
+                            label="姓" 
+                            variant="outlined" 
+                            InputProps={{startAdornment:(<InputAdornment position="start"><Alphabetical color="action"/> : </InputAdornment>)}}
+                        />
+                        <TextField 
+                            value={editTargetStudent.firstName} 
+                            onChange={(e) => handleTargetStudent(e, "firstName")} 
+                            style={{marginBottom:24, width:"100%"}} 
+                            label="名" 
+                            variant="outlined" 
+                            InputProps={{startAdornment:(<InputAdornment position="start"><Alphabetical color="action"/> : </InputAdornment>)}}
+                            />                        
                         <FormControl style={{marginBottom:24, width:"100%"}} variant="outlined">
                             <InputLabel variant="outlined">学部</InputLabel>
-                                <Select labelWidth={32} value={faculityId} onChange={handleChangeFaculityId}>
+                                <Select labelWidth={32} value={editTargetStudent.faculityId} onChange={(e) => handleTargetStudent(e, "faculityId")}>
                                     <MenuItem value={0}>未選択</MenuItem>
                                     <MenuItem value={1}>工学①</MenuItem>
                                     <MenuItem value={2}>理学②</MenuItem>
@@ -46,21 +108,22 @@ const InputDialog: React.FC = () => {
                         </FormControl>
                         <FormControl style={{marginBottom:24, width:"100%"}} variant="outlined">
                             <InputLabel variant="outlined">入学年度</InputLabel>
-                                <Select labelWidth={64} value={entranceYear} onChange={handleChangeEntranceYear}>
+                                <Select labelWidth={64} value={editTargetStudent.entranceYear} onChange={(e) => handleTargetStudent(e, "entranceYear")}>
                                     <MenuItem value={0}>未選択</MenuItem>
-                                    <MenuItem value={1}>2018</MenuItem>
-                                    <MenuItem value={2}>2019</MenuItem>
-                                    <MenuItem value={3}>2020</MenuItem>
+                                    <MenuItem value={"2018"}>2018年度</MenuItem>
+                                    <MenuItem value={"2019"}>2019年度</MenuItem>
+                                    <MenuItem value={"2020"}>2020年度</MenuItem>
                                 </Select>
                         </FormControl>
-                        
-                        {/* <TextField label="学部" variant="outlined" InputProps={{startAdornment:(<InputAdornment position="start"><TitleTwoTone/></InputAdornment>)}}/>
-                        <TextField label="入学年" variant="outlined" InputProps={{startAdornment:(<InputAdornment position="start"><LooksOneTwoTone/></InputAdornment>)}}/><TextField label="管理ID" variant="outlined" InputProps={{startAdornment:(<InputAdornment position="start"><LooksOneTwoTone/></InputAdornment>)}}/> */}
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={() => dispatch({type: ActionType.DIALOG_CLOSE})}>キャンセル</Button>
-                    <Button onClick={() => dispatch({type: ActionType.DIALOG_CLOSE})} color="secondary">登録</Button>
+                    <Button onClick={() => {
+                        dispatch({type: ActionType.DIALOG_UPDATE, payload:{...state, isShowDialog: false}})
+                        setTargetStudent(defaultStudent);
+                    }}
+                    >キャンセル</Button>
+                    <Button onClick={() => { dialogMode === "新規作成" ? postStudent() : putStudent(); dispatch({type: ActionType.DIALOG_UPDATE, payload:{...state, isShowDialog: false}})}} color="secondary">登録</Button>
                 </DialogActions>
             </Dialog>
         </div>
