058624bfaf3011fe4c757715d8f602d312018ef0
konno-yu <yuta.konno.socie@gmail.com>
2019-11-17 11:21:37 +0900

    [front] 学生の新規作成時に利用するデフォ値を定数化

--- src/main/front/src/const.ts
+++ src/main/front/src/const.ts
@@ -1,9 +1,19 @@
 export type StudentProperties = {
-    id: number;
+    studentId: number;
     studentNumber: string;
     lastName: string;
     firstName: string;
     faculityId: number;
     classId: number;
     entranceYear: string;
+};
+
+export const defaultStudent: StudentProperties = {
+    studentId: -1,
+    studentNumber: "",
+    lastName: "",
+    firstName: "",
+    faculityId: 0,
+    classId: 0,
+    entranceYear: "2019"
 };
