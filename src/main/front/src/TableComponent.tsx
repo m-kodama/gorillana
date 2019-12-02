3db23377303d4b6f24cb1a24b7192f9010dcd9b4
konno-yu <yuta.konno.socie@gmail.com>
2019-12-02 23:40:54 +0900

    [front] ハードコーディングしていた文言を定数を利用した書き方にしてみた

--- src/main/front/src/TableComponent.tsx
+++ src/main/front/src/TableComponent.tsx
@@ -2,13 +2,14 @@ import * as React from 'react';
 import { useContext } from 'react';
 import { Store } from './store';
 import { ActionType } from './reducer';
-import { Table, TableHead, TableRow, TableCell, TableBody, Button, Paper, Tooltip, Popover, Typography, Snackbar } from '@material-ui/core';
-import { EditTwoTone, DeleteTwoTone, PersonAddTwoTone, InsertDriveFileTwoTone } from '@material-ui/icons';
+import { Table, TableHead, TableRow, TableCell, TableBody, Button, Paper, Tooltip, Popover, Typography } from '@material-ui/core';
+import { EditTwoTone, DeleteTwoTone } from '@material-ui/icons';
 import { TrashCanOutline, AccountPlus, FileMove } from 'mdi-material-ui';
 import { StudentProperties, defaultStudent } from './const';
 import InputDialog from './InputDialog';
 import axios from 'axios';
 import _ from 'lodash';
+import { Gorillana } from './messageResource';
 
 type StudentProps = {
     children: StudentProperties[]
@@ -19,8 +20,18 @@ const TableComponent: React.FC<StudentProps> = (props: StudentProps) => {
     const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
     const [targetStudent, setTargetStudent] = React.useState<StudentProperties>(defaultStudent);
    
-    const headerElements = ["管理ID", "学籍番号", "姓", "名", "学部", "クラス", "入学年度", "", ""];
-    
+    const headerElements = [
+        Gorillana.STUDENT.STUDENT_ID, 
+        Gorillana.STUDENT.STUDENT_NUMBER, 
+        Gorillana.STUDENT.LAST_NAME, 
+        Gorillana.STUDENT.FIRST_NAME, 
+        Gorillana.STUDENT.FACULITY_NAME, 
+        Gorillana.STUDENT.CLASS, 
+        Gorillana.STUDENT.ENTRANCE_YEAR, 
+        "", 
+        ""
+    ]
+
     const students = state.students;
 
     const handleClickDelete = (event: React.MouseEvent<HTMLButtonElement>, row: StudentProperties) => {
@@ -55,11 +66,11 @@ const TableComponent: React.FC<StudentProps> = (props: StudentProps) => {
             }}>
                 <div style={{margin:15, display:"flex", alignItems:"center"}}>
                     <TrashCanOutline color="secondary"/>
-                    <Typography style={{fontWeight:"bold", marginLeft:5, fontSize:14}}>本当に削除しますか？</Typography>
+        <Typography style={{fontWeight:"bold", marginLeft:5, fontSize:14}}>{Gorillana.COMMON.CONFIRM_DELETE}</Typography>
                 </div>
                 <div style={{display:"flex", margin:10, flexDirection:"row-reverse"}}>
-                    <Button size="small" onClick={deleteStudent} color="secondary">OK</Button>
-                    <Button size="small" onClick={() => setAnchorEl(null)}>キャンセル</Button>
+        <Button size="small" onClick={deleteStudent} color="secondary">{Gorillana.COMMON.OK}</Button>
+                    <Button size="small" onClick={() => setAnchorEl(null)}>{Gorillana.COMMON.CANCEL}</Button>
                 </div>
             </Popover>
         );
@@ -68,8 +79,8 @@ const TableComponent: React.FC<StudentProps> = (props: StudentProps) => {
     return (
         <div>
             <div style={{textAlign:"center", margin:"15px 0px"}}>
-                <Button style={{margin:"5px 5px", boxShadow:"none"}} variant="contained" color="secondary" startIcon={<AccountPlus/>} onClick={() => { setTargetStudent(defaultStudent);dispatch({type: ActionType.DIALOG_UPDATE, payload:{...state, isShowDialog: true}})}}>新規作成</Button>
-                <Button style={{margin:"5px 5px", backgroundColor:"#FFFFFF"}} variant="outlined" startIcon={<FileMove/>}>CSVから新規作成</Button>
+                <Button style={{margin:"5px 5px", boxShadow:"none"}} variant="contained" color="secondary" startIcon={<AccountPlus/>} onClick={() => { setTargetStudent(defaultStudent);dispatch({type: ActionType.DIALOG_UPDATE, payload:{...state, isShowDialog: true}})}}>{Gorillana.STUDENT.CREATE}</Button>
+    <Button style={{margin:"5px 5px", backgroundColor:"#FFFFFF"}} variant="outlined" startIcon={<FileMove/>}>{Gorillana.STUDENT.CREATE_FROM_CSV}</Button>
             </div>
             <Paper style={{width:"90%", margin:"0 auto"}}>
                 { Boolean(anchorEl) && showConfirmPopOver() }
@@ -87,8 +98,8 @@ const TableComponent: React.FC<StudentProps> = (props: StudentProps) => {
                                     return (
                                         <TableRow>
                                             {_.map(row).map(cell => <TableCell style={{textAlign:"center"}}>{cell}</TableCell>)}
-                                            <Tooltip title="学生を編集する"><TableCell style={{textAlign:"center"}}><Button onClick={(e) => {setTargetStudent(row);dispatch({type: ActionType.DIALOG_UPDATE, payload:{...state, isShowDialog: true}})}}><EditTwoTone/></Button></TableCell></Tooltip>
-                                            <Tooltip title="学生を削除する"><TableCell style={{textAlign:"center"}}><Button onClick={(e) => handleClickDelete(e, row)}><DeleteTwoTone/></Button></TableCell></Tooltip>
+                                            <Tooltip title={Gorillana.STUDENT.EDIT}><TableCell style={{textAlign:"center"}}><Button onClick={(e) => {setTargetStudent(row);dispatch({type: ActionType.DIALOG_UPDATE, payload:{...state, isShowDialog: true}})}}><EditTwoTone/></Button></TableCell></Tooltip>
+                                            <Tooltip title={Gorillana.STUDENT.DELETE}><TableCell style={{textAlign:"center"}}><Button onClick={(e) => handleClickDelete(e, row)}><DeleteTwoTone/></Button></TableCell></Tooltip>
                                         </TableRow>
                                     )
                                 })
