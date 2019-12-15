2b5a4b8c8f95a69a8198122d2722c5dfa8dac312
Masaaki Kodama <tc003ssr@gmail.com>
2019-12-15 21:33:58 +0900

    feat: 学部テーブル作成 #10

--- docker/var/lib/postgres/init/1_init.sql
+++ docker/var/lib/postgres/init/1_init.sql
@@ -16,3 +16,20 @@ INSERT INTO students (student_number, last_name, first_name, faculity_id, class_
   ('GOA1001', '門脇', '沙耶', 1, 1, '2019'),
   ('GOB1001', '西原', '徹', 2, 4, '2019'),
   ('GOC1001', '梅津', '憲仁', 3, 7, '2019');
+
+-- 学部テーブル
+DROP TABLE IF EXISTS faculities;
+
+CREATE TABLE faculities (
+  faculity_id SERIAL NOT NULL,
+  faculity_name VARCHAR(255) NOT NULL,
+  faculity_name_short VARCHAR(5),
+  PRIMARY KEY (faculity_id)
+);
+
+INSERT INTO faculities (faculity_name, faculity_name_short) VALUES
+  ('文学部', '文'),
+  ('教育学部', '教'),
+  ('法学部', '法'),
+  ('経済学部', '経'),
+  ('理学部', '理');
